8c087ccc096625a0b43f03a4d66ccc9ffbf35ad5
xuweixiang <markhsu@systex.com>
2022-12-12 19:01:10 +0800

    新增鴻海 API

--- backend/app.py
+++ backend/app.py
@@ -32,6 +32,7 @@ app = Flask(__name__)
 app.debug=True
 CORS(app)
 
+#長榮股價
 @app.route('/chart_evergreen' , methods=['GET'])
 def get_chart_evergreen():
 
@@ -43,8 +44,22 @@ def get_chart_evergreen():
 
     return json.dumps(result , cls=DecimalEncoder ,ensure_ascii=False)
 
+
+#鴻海股價
+@app.route('/chart_foxconn' , methods=['GET'])
+def get_chart_foxconnconmment():
+
+    selectSQL = "SELECT year(datetime) AS year, MONTH(datetime) AS month ,sum(volume) AS sumVol ,round(avg(endprice),2) as avgClose,count(*) as countDay FROM foxconnprice GROUP BY  month,year order BY  datetime"
+
+    cursor.execute(selectSQL)
+
+    result = cursor.fetchall()
+
+    return json.dumps(result , cls=DecimalEncoder ,ensure_ascii=False)
+
+#長榮用戶月發文量
 @app.route('/evergreen/chart_1/<string:username>',methods=['GET'])
-def get_chart1(username):
+def get_evergreenchart1(username):
 
     name = str(username)
 
@@ -56,8 +71,23 @@ def get_chart1(username):
 
     return json.dumps(result , cls = DecimalEncoder ,ensure_ascii=False)
 
+#鴻海用戶月發文量
+@app.route('/foxconn/chart_1/<string:username>',methods=['GET'])
+def get_foxconnchart1(username):
+
+    name = str(username)
+
+    selectSQL = "SELECT year(datetime) AS year, MONTH(datetime) AS month ,username,count(*) as volOfMonth FROM `foxconncomment` where  username = " + "'"  + "%s" + "'"  + " GROUP BY year,month,username order BY  datetime,username"
+
+    cursor.execute(selectSQL %name)
+
+    result = cursor.fetchall()
+
+    return json.dumps(result , cls = DecimalEncoder ,ensure_ascii=False)
+
+#長榮用戶在一段時間的文字雲
 @app.route('/evergreen/wordcloud/<string:username>')
-def get_wordcould(username):
+def get_evergreenwordcould(username):
 
     starttime=request.args.get('starttime')
     endtime=request.args.get('endtime')
@@ -92,8 +122,45 @@ def get_wordcould(username):
     return(json.dumps(sorted_word_cnt))
 
 
+#鴻海用戶在一段時間的文字雲
+@app.route('/foxconn/wordcloud/<string:username>')
+def get_foxconnwordcould(username):
+
+    starttime=request.args.get('starttime')
+    endtime=request.args.get('endtime')
+
+    selectSQL = "SELECT comment FROM `foxconncomment` where  username = " + "'"  + "%s" + "'"  + " AND datetime between "+ "'"  + "%s" + "'" + "AND" + "'"  + "%s" + "'" 
+    
+    cursor.execute(selectSQL %(username ,starttime ,endtime))
+
+    result = cursor.fetchall()
+
+    totalstring=''
+
+    for dic in result:
+        totalstring+=dic['comment']
+
+    onlychinesestring = re.sub ('[^\u4e00-\u9fa5]','',totalstring)
+
+    sentence_list = re.split(r'[^\w ]', onlychinesestring)
+
+    word_list=[]
+    for i in sentence_list:
+        seg_list = jieba.lcut(i)
+        for r in seg_list:
+            if r not in stopwords and r != ' ':
+                word_list.append(r)
+    word_cnt = {}  #定義字典，鍵:值
+    for word in word_list:       
+        word_cnt[word] = word_cnt.get(word, 0) + 1
+
+    sorted_word_cnt = sorted(word_cnt.items(), key=lambda kv: kv[1], reverse=True)
+
+    return(json.dumps(sorted_word_cnt))
+
+#長榮一個月的發文量前五名
 @app.route('/evergreen/ranking')
-def get_rankbymonth():
+def get_evergreenrankbymonth():
 
     year=request.args.get('year')
     month=request.args.get('month')
@@ -108,8 +175,29 @@ def get_rankbymonth():
 
     return json.dumps(result , cls = DecimalEncoder ,ensure_ascii=False)
 
+
+
+#鴻海一個月的發文量前五名
+@app.route('/foxconn/ranking')
+def get_foxconnrankbymonth():
+
+    year=request.args.get('year')
+    month=request.args.get('month')
+
+    
+    selectSQL = "SELECT * FROM (select YEAR(datetime) as 年份,month(datetime) as 月份,username ,count(*) as commentbymonth , SUM(commentlike) , SUM(commentresponse) , ROW_NUMBER() over (order by COUNT(*) DESC) as ranking from foxconncomment WHERE (YEAR(datetime) = " + "'" + "%s" + "'" + "AND MONTH(datetime) = " + "%s" + " ) GROUP BY 年份,月份,username order by ranking ) AS a WHERE a.ranking <=5"
+
+
+    cursor.execute(selectSQL %(year , month))
+
+    result = cursor.fetchall()
+
+    return json.dumps(result , cls = DecimalEncoder ,ensure_ascii=False)
+
+
+#長榮用戶在一個月內發文的篇數
 @app.route('/evegreen/dailypost/<string:username>')
-def get_dailypost(username):
+def get_evergreendailypost(username):
 
     year=request.args.get('year')
     month=request.args.get('month')
@@ -120,12 +208,28 @@ def get_dailypost(username):
 
     result = cursor.fetchall()
 
-    print(result)
+    return json.dumps(result , cls = DecimalEncoder ,ensure_ascii=False ,default=str)
+
+
+#鴻海用戶在一個月內發文的篇數
+@app.route('/foxconn/dailypost/<string:username>')
+def get_foxconndailypost(username):
+
+    year=request.args.get('year')
+    month=request.args.get('month')
+
+    selectSQL = "SELECT DATE(DATETIME) AS 日期 ,username, COUNT(`comment`) AS 日發文數 FROM foxconncomment WHERE username = " + "'" + "%s" +  "'" + "AND YEAR(DATETIME) = " + "'" + "%s" + "'" + " AND MONTH(DATETIME) = " + "'" + "%s" + "'" + "GROUP BY  DATE(datetime)"
+
+    cursor.execute(selectSQL %(username ,year , month))
+
+    result = cursor.fetchall()
 
     return json.dumps(result , cls = DecimalEncoder ,ensure_ascii=False ,default=str)
 
+
+#長榮一個月全部發文的詞頻
 @app.route('/evegreen/monthlywordcloud')
-def get_monthlywordcloud():
+def get_evergreenmonthlywordcloud():
 
     year=request.args.get('year')
     month=request.args.get('month')
@@ -159,6 +263,44 @@ def get_monthlywordcloud():
 
     return(json.dumps(sorted_word_cnt))
 
+
+#鴻海一個月全部發文的詞頻
+@app.route('/foxconn/monthlywordcloud')
+def get_foxconnmonthlywordcloud():
+
+    year=request.args.get('year')
+    month=request.args.get('month')
+
+    selectSQL = "SELECT comment FROM `foxconncomment` where  YEAR(DATETIME) = " + "'" + "%s" + "'" + " AND MONTH(DATETIME) = " + "'" + "%s" + "'"
+
+    cursor.execute(selectSQL %(year ,month))
+
+    result = cursor.fetchall()
+
+    totalstring=''
+
+    for dic in result:
+        totalstring+=dic['comment']
+
+    onlychinesestring = re.sub ('[^\u4e00-\u9fa5]','',totalstring)
+
+    sentence_list = re.split(r'[^\w ]', onlychinesestring)
+
+    word_list=[]
+    for i in sentence_list:
+        seg_list = jieba.lcut(i)
+        for r in seg_list:
+            if r not in stopwords and r != ' ':
+                word_list.append(r)
+    word_cnt = {}  #定義字典，鍵:值
+    for word in word_list:       
+        word_cnt[word] = word_cnt.get(word, 0) + 1
+
+    sorted_word_cnt = sorted(word_cnt.items(), key=lambda kv: kv[1], reverse=True)
+
+    return(json.dumps(sorted_word_cnt))
+
+#預測文章類型
 @app.route('/predict', methods=['POST'])
 def postInput():
     # 取得前端傳過來的數值
@@ -170,8 +312,10 @@ def postInput():
     result = model.predict(input)
     return jsonify({'發文者類型': result,'情緒意味':"看漲看跌"})
 
+
+#長榮一個月內用戶的發文頻率
 @app.route('/evergreen/user/monthlywordcloud')
-def get_usermonthlywordcloud():
+def get_evergreenusermonthlywordcloud():
 
     year=request.args.get('year')
     month=request.args.get('month')
@@ -184,6 +328,23 @@ def get_usermonthlywordcloud():
 
     return jsonify(result)
 
+#鴻海一個月內用戶的發文頻率
+@app.route('/foxconn/user/monthlywordcloud')
+def get_foxconnusermonthlywordcloud():
+
+    year=request.args.get('year')
+    month=request.args.get('month')
+
+    selectSQL = "SELECT username , count(*) AS 當月發文數 FROM `foxconncomment` where  YEAR(DATETIME) = " + "'" + "%s" + "'" + " AND MONTH(DATETIME) = " + "'" + "%s" + "'" + " GROUP BY username ORDER BY count(*) DESC"
+
+    cursor.execute(selectSQL %(year ,month))
+
+    result = cursor.fetchall()
+
+    return jsonify(result)
+
+
+#長榮一個月內的每日價格
 @app.route('/evergreen/dailyprice')
 def get_evergreenprice():
 
@@ -198,6 +359,22 @@ def get_evergreenprice():
 
     return json.dumps(result , cls = DecimalEncoder ,ensure_ascii=False ,default=str)
 
+#鴻海一個月內的每日價格
+@app.route('/foxconn/dailyprice')
+def get_foxconnprice():
+
+    year=request.args.get('year')
+    month=request.args.get('month')
+
+    selectSQL = "SELECT DATE(DATETIME) , endprice FROM foxconnprice WHERE YEAR(DATETIME) =" + "'" + "%s" + "'" + " AND MONTH(DATETIME) = " + "'" + "%s" + "'"
+
+    cursor.execute(selectSQL %(year ,month))
+
+    result = cursor.fetchall()
+
+    return json.dumps(result , cls = DecimalEncoder ,ensure_ascii=False ,default=str)
+
+#長榮用戶一天的發文類型
 @app.route('/evergreen/predict/people', methods=['POST'])
 def predictevergreenbypeople():
 
@@ -236,8 +413,49 @@ def predictevergreenbypeople():
 
     return jsonify({'發文者類型':result,"機器人文章數":a,"分析師文章數":b,"散戶文章數":c})
 
+
+#鴻海用戶一天的發文類型
+@app.route('/foxconn/predict/people', methods=['POST'])
+def predictfoxconnbypeople():
+
+    insertValues = request.get_json()
+    username=insertValues['username']
+    date=insertValues['date']
+
+    cursor.execute(f"SELECT comment FROM `foxconncomment` where username = \'{username}\' and date(datetime) = \'{date}\'")
+
+    result = cursor.fetchall()
+
+    a=0 ;b=0;c=0
+    for i in range(len(result)):
+        input_tuple=result[i]
+        input=str(input_tuple)
+        result_pred=model.predict(input)
+        if result_pred=="機器人":
+            a=a+1
+        elif result_pred=="分析師":
+            b=b+1
+        else:
+            c=c+1
+    #比大小
+    if a>b:
+        if a>c:
+            result='機器人'
+        else:
+            result='散戶'
+    elif a<b:
+        if b>c:
+            result='分析師'
+        else:
+            result='散戶'
+    else:
+        result='一樣'
+
+    return jsonify({'發文者類型':result,"機器人文章數":a,"分析師文章數":b,"散戶文章數":c})
+
+#長榮用戶發文情緒字數計算
 @app.route('/evergreen/ranking/emotion/<string:username>')
-def getrankingpeopleemotion(username):
+def getevergreenrankingpeopleemotion(username):
 
     year=request.args.get('year')
     month=request.args.get('month')
@@ -262,7 +480,47 @@ def getrankingpeopleemotion(username):
             if r not in stopwords and r != ' ':
                 word_list.append(r)
 
-    print(word_list)
+    poswordstimes=0
+    negwordstimes=0
+    stawordstimes=0
+    for word in word_list:
+        if word in poswords:
+            poswordstimes+=1
+        elif word in negwords:
+            negwordstimes+=1
+        elif word in stawords:
+            stawordstimes+=1
+
+    return jsonify({ "正向字詞次數" : poswordstimes , "負向字詞次數" : negwordstimes , "中立字詞次數":stawordstimes})
+
+
+#鴻海用戶發文情緒字數計算
+@app.route('/foxconn/ranking/emotion/<string:username>')
+def getfoxconnrankingpeopleemotion(username):
+
+    year=request.args.get('year')
+    month=request.args.get('month')
+
+    cursor.execute(f"SELECT comment FROM `foxconncomment` where username = \'{username}\' and year(datetime) = \'{year}\' and month(datetime) = \'{month}\'")
+
+    result = cursor.fetchall()
+
+    totalstring=''
+
+    for dic in result:
+        totalstring+=dic['comment']
+
+    onlychinesestring = re.sub ('[^\u4e00-\u9fa5]','',totalstring)
+
+    sentence_list = re.split(r'[^\w ]', onlychinesestring)
+
+    word_list=[]
+    for i in sentence_list:
+        seg_list = jieba.lcut(i)
+        for r in seg_list:
+            if r not in stopwords and r != ' ':
+                word_list.append(r)
+
     poswordstimes=0
     negwordstimes=0
     stawordstimes=0
